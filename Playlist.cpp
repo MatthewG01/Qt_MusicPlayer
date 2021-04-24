73b98f76a570d2e0cfad8d70bb72ed07b91d372c
MatthewG01 <matthewgazzard2@yahoo.co.uk>
2021-04-24 15:22:24 +0100

    Final: Added comments to explain functions

--- Playlist.cpp
+++ Playlist.cpp
@@ -6,31 +6,37 @@ Playlist::Playlist(QObject *Widget, QString name)
     playlistName = name;
 }
 
+//Allows user to name created playlist in newPlaylist()
 void Playlist::setName(QString aName)
 {
     playlistName = aName;
 }
 
+//Retrieves the name of the playlist so that it can be displayed and have its name used to access corresponding contents file
 QString Playlist::getName()
 {
     return playlistName;
 }
 
+//Allows you to set the playlist being used by the player by incorporating getPlaylist()
 void Playlist::setPlaylist(QMediaPlaylist *aPlaylist)
 {
     playlist = aPlaylist;
 }
 
+//Gets the playlist
 QMediaPlaylist *Playlist::getPlaylist()
 {
     return playlist;
 }
 
+//Sets the playlist tracks
 void Playlist::setTracks(QVector<QString> tracksToSet)
 {
     tracks = tracksToSet;
 }
 
+//Retrieve playlist tracks to be used in current track line edit
 QString Playlist::getTrack(int index)
 {
     return tracks[index];
